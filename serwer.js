041f3032388f03877b68f4208f830606452144f9
JohnyXDDD <jan.lenart.jl@gmail.com>
2024-05-26 18:56:10 +0200

    first commit

---
+++ serwer.js
@@ -0,0 +1,36 @@
+const port = 3010 || process.env.PORT
+const express = require('express')
+const cors = require('cors')
+// require('dotenv').config()
+const app = express()
+app.use(cors())
+// {
+//     'origin':"*",
+//     'Content-Type': 'text/plain',
+//     'Access-Control-Allow-Origin' : '*',
+//     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+// // }
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next()
+// })
+app.get('/', (req, res) => {
+    const paymentId = req.query.paymentId;
+    const randomNo = Math.floor(Math.random() * 2) + 1;
+    let status = ''
+    if (randomNo == 1) {
+        status = 'canceled'
+    }
+    else if (randomNo == 2) {
+        status = 'confirmed'
+    }
+    else {
+        status = 'waiting'
+    }
+    res.send({
+        paymentId: paymentId,
+        status: status
+    })
+    // res.redirect(301, 'https://www.interia.pl');
+})
+app.listen(port, () => console.log(`Server running on port ${port}`))
